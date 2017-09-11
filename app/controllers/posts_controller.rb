b322a07242a38a03b861ebefeab963ec6bdb4b6b
Your Name <you@example.com>
2017-09-11 18:15:47 +0800

    user simple_format helper to decorate

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,32 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to account_posts_path, notice: "更新成功！"
+    else
+      render :eidt
+    end
+  end
+
+
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to account_posts_path, alert: "删除成功！"
+  end
+
+
+
   private
 
   def post_params
